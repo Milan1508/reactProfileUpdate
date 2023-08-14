0a8bb857a86b4a21ee6074001e9b0395bf27ed76
Milan Sood <milansood15@gmail.com>
2023-08-14 15:56:01 +0530

    trying something

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])  // [ {id: 1, text: "item1", completed: false}, {id: 2, text: "item2", completed: false}
+
+  function handleSubmit(e) {
+    e.preventDefault()  // prevent page from reloading
+
+    setTodos([
+      ...todos, { id: crypto.randomUUID(), title: newItem, completed: false }])
+  }
+
+  console.log(todos)
+
+  // setNewItem("sjbvdf")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}
+        className='new-item-form'>
+        <div className="form-row">
+          <label htmlFor='item'>New Item</label>
+          <input value={newItem}
+            onChange={e => setNewItem(e.target.value)}
+            type="text" id='item' />
+        </div>
+
+        <button className='btn'>Add</button>
+      </form>
+      <h1 className='header'>My To Do List</h1>
+      <ul className='list'>
+        <li>
+          <label>
+            <input type='checkbox' />
+            Item1
+          </label>
+          <button className='btn btn-danger'>Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type='checkbox' />
+            Item2
+          </label>
+          <button className='btn btn-danger'>Delete</button>
+
+        </li>
+      </ul>
+    </>
   );
 }
 

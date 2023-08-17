fbb196c29d57589f77b1ec4ec4cfa3de78443f1d
Milan Sood <milansood15@gmail.com>
2023-08-17 12:18:06 +0530

    adding new page for get getPatient

---
+++ src/getPatient.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const getPatient = () => {
+    return (
+        <div>
+            <h1>Submit </h1>
+            <p> Patient Id for getting the information</p>
+        </div>
+    );
+};
+
+export default getPatient;
